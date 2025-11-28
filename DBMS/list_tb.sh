a4bb50c0e1a904fe1bcf1d60a3ddf49a495ebba3
codsalah <salahhamada63@gmail.com>
2025-11-28 12:50:14 +0200

    Add: Implement a script to list tables

--- DBMS/list_tb.sh
+++ DBMS/list_tb.sh
@@ -1,32 +1,35 @@
 #!/usr/bin/bash
 
-read -p "Do you want to list tables with its metadata? (y/n): " list_metadata
-
-# Convert input to lowercase for easier comparison
-input=$(echo "$list_metadata" | tr '[:upper:]' '[:lower:]')
-
-echo -e "\nExisting Tables:"
-echo "----------------"
-
 # List files in current directory, excluding subdirectories
 files=$(ls -p | grep -v /)
 
-# only accept yes or y as input 
-if [[ "$input" == "y" || "$input" == "yes" ]]; then
-    # Show all files (tables and metadata)
-    tables="$files"
-# only accept n or no as input  
-elif [[ "$input" == "n" || "$input" == "no" ]]; then
-    # Exclude .meta files if no
-    tables=$(echo "$files" | grep -v "\.meta$")
-else
-    echo "Invalid input. Only 'y/yes' or 'n/no' are accepted."
-    exit 1
-fi
-
-if [[ -z "$tables" ]]; then
+if [[ -z "$files" ]]; then
+echo "-------------------"
+    echo "No Existing Tables"
+    echo "-------------------"
     echo "No tables found."
 else
-    echo "$tables"
+    read -p "Do you want to list tables with its metadata? (y/n): " list_metadata
+    # Convert input to lowercase for easier comparison
+    input=$(echo "$list_metadata" | tr '[:upper:]' '[:lower:]')
+    # only accept yes or y as input 
+    if [[ "$input" == "y" || "$input" == "yes" ]]; then
+        # Show all files (tables and metadata)
+        echo -e "\nExisting Tables with its metadata:"
+        echo "----------------"
+        tables="$files"
+        echo $tables
+    # only accept n or no as input  
+    elif [[ "$input" == "n" || "$input" == "no" ]]; then
+        # Exclude .meta files if no
+        echo -e "\nExisting Tables:"
+        echo "----------------"
+        tables=$(echo "$files" | grep -v "\.meta$")
+        echo $tables
+    else
+        echo "Invalid input. Only 'y/yes' or 'n/no' are accepted."
+        exit 1
+    fi
 fi
-echo "----------------"
+echo "-------------------"
+
